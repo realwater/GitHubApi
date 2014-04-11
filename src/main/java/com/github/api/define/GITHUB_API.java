55f3b4b6319486f749330bba83f9e584f03c4f81
박진수 <@realwater>
2014-04-11 10:09:07 +0900

    fasdasd

---
+++ src/main/java/com/github/api/define/GITHUB_API.java
@@ -0,0 +1,16 @@
+package com.github.api.define;
+
+public enum GITHUB_API {
+
+	APP_CLIENT_ID("046fbf1a5040af820ffd");
+	//APP_CLIENT_SECRET("6af73b5759d45956b58afdb9e2a86e8bbaea28d1");
+
+	public String type;
+
+	private GITHUB_API(String type){
+		this.type = type;
+	}
+
+
+
+}
---
+++ src/main/java/com/github/api/define/Test.java
@@ -0,0 +1,12 @@
+package com.github.api.define;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Currency usCoin = Currency.DIME;
+		System.out.println(usCoin);
+	}
+}
