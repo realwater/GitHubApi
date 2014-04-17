2b6b762e938bbda48187079bc8af715b5e4c124e
박진수 <@realwater>
2014-04-17 22:04:41 +0900

    - 리파지토리 연결 설정

--- src/main/java/com/flyJoin/gitHubApi/controller/RepositoryController.java
+++ src/main/java/com/flyJoin/gitHubApi/controller/RepositoryController.java
@@ -1,7 +1,7 @@
 package com.flyJoin.gitHubApi.controller;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,16 +12,16 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
-import com.flyJoin.gitHubApi.http.RestTemplateRequestManager;
-import com.flyJoin.gitHubApi.model.ResultInfo;
+import com.flyJoin.gitHubApi.model.Repos;
 
 @Controller
 @RequestMapping("/repos")
 public class RepositoryController {
 
 	@Autowired
-	RestTemplateRequestManager restTemplateRequestManager;
+	RestTemplate restTemplate;
 
 	/**
 	 * 저장소 분석
@@ -37,35 +37,59 @@ public class RepositoryController {
 	}
 
 	/**
-	 * 저장소 목록 정보
+	 * 저장소 디렉토리 정보
 	 * 2014.04.16
 	 * @author realwater
 	 */
-	@RequestMapping(value="/{id}/{project}/info/**", method=RequestMethod.GET)
-	public void repositoryInfo(
+	@RequestMapping(value="/{id}/{project}/dir/info/**", method=RequestMethod.GET)
+	public void repositoryDirectoryInfo(
 			@PathVariable("id") String id,
 			@PathVariable("project") String project,
 			HttpServletRequest request,
 			ModelMap model) {
 
 		// Git 저장소 path 구해오기
-		String repositoryPath = request.getRequestURI().replace("/repos/"+id+"/"+project+"/info", "");
+		String repositoryPath = request.getRequestURI().replace("/repos/"+id+"/"+project+"/dir/info", "");
 
-		System.out.println(id);
-		System.out.println(project);
-		List<HashMap<String,Object>> latestListJson = new ArrayList<HashMap<String,Object>>();
-		ResultInfo sendDataResult = new ResultInfo();
-		String url = "https://api.github.com/repos/realwater/GitHubApi/contents/src/main/java/com/github/api/controller/HomeController.java";
+		List<Repos> repoArrayList = new ArrayList<Repos>();
+		String url = "https://api.github.com/repos/realwater/GitHubApi/contents/src/main/java/com/flyJoin/gitHubApi/controller";
+
 		try{
-			sendDataResult = restTemplateRequestManager.getExchange(url, request);
-			if(sendDataResult != null && sendDataResult.getData() != null){
-				latestListJson =   (List<HashMap<String,Object>>) sendDataResult.getData();
-			}
+			Repos[] reposList = restTemplate.getForObject(url, Repos[].class);
+			repoArrayList = Arrays.asList(reposList);
 		}catch( Exception e){
 			e.printStackTrace();
 		}
 
 		model.clear();
-		model.addAttribute(latestListJson);
+		model.addAttribute(repoArrayList);
+	}
+
+	/**
+	 * 저장소 파일 정보
+	 * 2014.04.16
+	 * @author realwater
+	 */
+	@RequestMapping(value="/{id}/{project}/file/info/**", method=RequestMethod.GET)
+	public void repositoryFileInfo(
+			@PathVariable("id") String id,
+			@PathVariable("project") String project,
+			HttpServletRequest request,
+			ModelMap model) {
+
+		// Git 저장소 path 구해오기
+		String repositoryPath = request.getRequestURI().replace("/repos/"+id+"/"+project+"/file/info", "");
+
+		Repos repo = new Repos();
+		String url = "https://api.github.com/repos/realwater/GitHubApi/contents/src/main/java/com/flyJoin/gitHubApi/controller/HomeController.java";
+
+		try{
+			repo = restTemplate.getForObject(url, Repos.class);
+		}catch( Exception e){
+			e.printStackTrace();
+		}
+
+		model.clear();
+		model.addAttribute(repo);
 	}
 }
---
+++ src/main/java/com/flyJoin/gitHubApi/model/Repos.java
@@ -0,0 +1,79 @@
+package com.flyJoin.gitHubApi.model;
+
+import org.apache.commons.codec.binary.Base64;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Repos {
+
+	private String name;
+
+	private String path;
+
+	private String sha;
+
+	private String size;
+
+	private String content;
+
+	private String encoding;
+
+	private String type;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String getSha() {
+		return sha;
+	}
+
+	public void setSha(String sha) {
+		this.sha = sha;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		byte[] decodeCont = Base64.decodeBase64(content.getBytes());
+		this.content = new String(decodeCont);
+	}
+
+	public String getEncoding() {
+		return encoding;
+	}
+
+	public void setEncoding(String encoding) {
+		this.encoding = encoding;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+}
