83419c0dff59aef9318f356340a5c20647b7a56d
realwater <realwater@danawa.com>
2014-06-22 23:48:17 +0900

    - 분석 클래스 추가

---
+++ src/main/java/org/flyJenkins/analysis/strategy/JAVASCRIPTAnalysisStrategy.java
@@ -0,0 +1,28 @@
+package org.flyJenkins.analysis.strategy;
+
+import org.flyJenkins.github.command.GitHubRepoCmd;
+import org.flyJenkins.github.model.ProjectDto;
+import org.flyJenkins.github.model.SearchCodeDto;
+import org.flyJenkins.github.service.GitHubRepoManager;
+
+public class JAVASCRIPTAnalysisStrategy implements ProjectAnalysisStrategy {
+	
+	private GitHubRepoManager gitHubRepoManager;
+	
+	public void setGitHubRepoManager(GitHubRepoManager gitHubRepoManager) {
+		this.gitHubRepoManager = gitHubRepoManager;
+	}
+	@Override
+	public void getGitAnalysisInfo(GitHubRepoCmd gitHubRepoCmd, ProjectDto projectDto) {
+		// Project가 node.js 인지 체크
+		gitHubRepoCmd.setQuery("version");
+		gitHubRepoCmd.setLanguage("json");
+
+		SearchCodeDto searchCodeDto = gitHubRepoManager.getSearchFileCode(gitHubRepoCmd);
+		if (searchCodeDto.getTotal_count() > 0) {
+			projectDto.setAnalysisChance("Y");
+			projectDto.setProjectType("node.js");
+		}
+	}
+
+}
